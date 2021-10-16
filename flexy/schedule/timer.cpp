fd78cf3568e0a10f25fe51ebe9c4f5d8e02707fd
4kangjc <1158035520@qq.com>
2021-10-16 23:23:03 +0800

    add timer

---
+++ flexy/schedule/timer.cpp
@@ -0,0 +1,161 @@
+#include "timer.h"
+#include "flexy/util/util.h"
+
+namespace flexy {
+
+bool Timer::Comparator::operator()(const Timer::ptr& lhs, const Timer::ptr& rhs) const {
+    if (!lhs && !rhs) {
+        return false;
+    }
+    if (!lhs) {
+        return true;
+    }
+    if (!rhs) {
+        return false;
+    }
+    if (lhs->next_ != rhs->next_) {
+        return lhs->next_ < rhs->next_;
+    }
+    return lhs.get() < rhs.get(); 
+}
+
+Timer::Timer(uint64_t ms, __task&& cb, bool recurring, TimerManager* manager) 
+    : recurring_(recurring), ms_(ms), cb_(std::move(cb)), manager_(manager) {
+    next_ = GetTimeMs() + ms_;
+}
+
+Timer::Timer(uint64_t next) : next_(next) { }
+
+bool Timer::cacel() {
+    LOCK_GUARD(manager_->mutex_);
+    if (cb_) {
+        cb_ = nullptr;
+        auto it = manager_->timers_.find(shared_from_this());
+        manager_->timers_.erase(it);
+        return true;
+    }
+    return false;
+}
+
+bool Timer::refresh() {
+    LOCK_GUARD(manager_->mutex_);
+    if (!cb_) {
+        return false;
+    }
+    auto it = manager_->timers_.find(shared_from_this());
+    if (it == manager_->timers_.end()) {
+        return false;
+    }
+    manager_->timers_.erase(it);
+    next_ = GetTimeMs() + ms_;
+    manager_->timers_.insert(shared_from_this());
+    return true;
+}
+
+bool Timer::reset(uint64_t ms, bool from_now) {
+    if (ms == ms_ && !from_now) {
+        return true;
+    }
+    unique_lock<decltype(manager_->mutex_)> lock(manager_->mutex_);
+    if (!cb_) {
+        return false;
+    }
+    auto it = manager_->timers_.find(shared_from_this());
+    if (it == manager_->timers_.end()) {
+        return false;
+    }
+    manager_->timers_.erase(it);
+    uint64_t start = from_now ? GetTimeMs() : next_ - ms_;
+    ms_ = ms;
+    next_ = ms_ + start;
+    manager_->addTimer(shared_from_this(), lock);
+    return true;
+}
+
+TimerManager::TimerManager() {
+    previouseTime_ = GetTimeMs();
+}
+
+void TimerManager::addTimer(Timer::ptr& val, unique_lock<mutex>& lock) {
+    return addTimer(Timer::ptr(val), lock);
+}
+
+void TimerManager::addTimer(Timer::ptr&& val, unique_lock<mutex>& lock) {
+    auto it = timers_.insert(val).first;
+    bool at_front = (it == timers_.begin() && !tickled_);
+    if (at_front) {
+        tickled_ = true;
+    }
+    lock.unlock();
+    if (at_front) {
+        onTimerInsertedAtFront();
+    }
+}
+
+void TimerManager::OnTimer(std::weak_ptr<void()> weak_cond, __task&& cb) {
+    auto tmp = weak_cond.lock();
+    if (tmp) {
+        cb();
+    }
+}
+
+bool TimerManager::detectClockRollover(uint64_t now_ms) {
+    bool rollver = false;
+    if (now_ms < previouseTime_ && now_ms < (previouseTime_ - 60 * 60 * 1000)) {
+        rollver = true;
+    }
+    previouseTime_ = now_ms;
+    return rollver;
+}
+
+bool TimerManager::hasTimer() const {
+    LOCK_GUARD(mutex_);
+    return !timers_.empty();
+}
+
+uint64_t TimerManager::getNextTimer() {
+    LOCK_GUARD(mutex_);
+    tickled_ = true;
+    if (timers_.empty()) {
+        return ~0ull;
+    }
+    auto& next = *timers_.begin();
+    uint64_t now_ms = GetTimeMs();
+    if (now_ms >= next->next_) {
+        return 0;
+    } else {
+        return next->next_ - now_ms;
+    }
+}
+
+std::vector<__task> TimerManager::listExpiriedTimer() {
+    std::vector<__task> cbs;
+    std::vector<Timer::ptr> expired;
+    uint64_t now_ms = GetTimeMs();
+    LOCK_GUARD(mutex_);
+    if (timers_.empty()) {
+        return cbs;
+    }
+    bool roller = detectClockRollover(now_ms);
+    if (!roller && ((*timers_.begin())->next_ > now_ms)) {
+        return cbs;
+    }
+
+    Timer::ptr now_timer(new Timer(now_ms));
+    auto it = roller ? timers_.end() : timers_.upper_bound(now_timer);
+    expired.insert(expired.begin(), timers_.begin(), it);
+    timers_.erase(timers_.begin(), it);
+
+    for (auto& timer : expired) {
+        if (timer->recurring_) {
+            cbs.push_back(timer->cb_);
+            timer->next_ = timer->ms_ + now_ms;
+            timers_.insert(timer);
+        } else {
+            cbs.push_back(std::move(timer->cb_));
+        }
+    }  
+    return cbs;
+}
+
+}
--- flexy/schedule/timer.h
+++ flexy/schedule/timer.h
@@ -1,2 +1,82 @@
 #pragma once
+#include <memory>
+#include <set>
+#include "flexy/util/task.h"
+#include "flexy/thread/mutex.h"
+namespace flexy {
 
+class TimerManager;
+class Timer : public std::enable_shared_from_this<Timer> {
+friend class TimerManager;
+public:
+    using ptr = std::shared_ptr<Timer>;
+    bool cacel();                                   // 取消定时器
+    bool refresh();                                 // 刷新设置定时器的执行时间
+    bool reset(uint64_t ms, bool from_now);         // 重置定时器时间 from_now 是否从当前时间开始计算
+private:
+    Timer(uint64_t ms, __task&& cb, bool recurring, TimerManager* manager);
+    Timer(uint64_t next);
+private:
+    bool recurring_;                                // 是否为循环定时器
+    uint64_t ms_;                                   // 执行周期
+    uint64_t next_;                                 // 精确的执行时间
+    __task cb_;                                     // 回调函数
+    TimerManager* manager_ = nullptr;               // 定时器所属定时器管理者
+private:
+    struct Comparator {
+        bool operator()(const Timer::ptr& lhs, const Timer::ptr& rhs) const;
+    };
+};
+
+class TimerManager {
+friend class Timer;
+public:
+    TimerManager();
+    virtual ~TimerManager() { }
+
+    template <typename... Args>
+    Timer::ptr addTimer(uint64_t ms, Args&&... args) {
+        __task t(std::forward<Args>(args)...);
+        Timer::ptr timer(new Timer(ms, std::move(t), false, this));
+        unique_lock<decltype(mutex_)> lock(mutex_);
+        addTimer(timer, lock);
+        return timer;
+    }
+
+    template <typename... Args>
+    Timer::ptr addRecTimer(uint64_t ms, Args&&... args) {
+        __task t(std::forward<Args>(args)...);
+        Timer::ptr timer(new Timer(ms, std::move(t), true, this));
+        unique_lock<decltype(mutex_)> lock(mutex_);
+        addTimer(timer, lock);
+        return timer;
+    }
+
+    template <typename... Args>
+    Timer::ptr addCondtionTimer(uint64_t ms, std::weak_ptr<void()> weak_cond, Args&&... args) {
+        return addTimer(ms, OnTimer, weak_cond, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    Timer::ptr addRecCondtionTimer(uint64_t ms, std::weak_ptr<void()> weak_cond, Args&&... args) {
+        return addRecTimer(ms, OnTimer, weak_cond, std::forward<Args>(args)...);
+    }
+
+    uint64_t getNextTimer();                                    // 获取下一个要执行的定时器任务的时间
+    std::vector<__task> listExpiriedTimer();                    // 获取已到期的定时器需要执行回调函数的列表
+    bool hasTimer() const;                                      // 是否有定时器
+protected:
+    virtual void onTimerInsertedAtFront() = 0;                  // 当有新的定时器插入到定时器的首部,执行该函数
+    void addTimer(Timer::ptr& val, unique_lock<mutex>& lock);     // 将定时器添加到timers_中
+    void addTimer(Timer::ptr&& val, unique_lock<mutex>& lock);    // 将定时器添加到timers_中
+    bool detectClockRollover(uint64_t now_ms);                  // 检测服务器时间是否被调后了
+private:
+    mutable mutex mutex_;                                       // 锁
+    std::set<Timer::ptr, Timer::Comparator> timers_;            // 定时器集合
+    bool tickled_ = false;                                      // 是否触发onTimerInsertedAtFront
+    uint64_t previouseTime_;                                    // 上次执行时间
+
+    static void OnTimer(std::weak_ptr<void()> weak_cond, __task&& cb);
+};
+
+} // namespace flexy
