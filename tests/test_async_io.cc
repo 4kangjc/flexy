27c654e1914e4255318cd0ff70793533a437e7d7
4kangjc <1158035520@qq.com>
2022-01-11 21:53:39 +0800

    add async_io test

---
+++ tests/test_async_io.cc
@@ -0,0 +1,27 @@
+#include <flexy/schedule/async_io.h>
+
+using flexy::async_cin;
+using flexy::async_cout;
+
+void test() {
+    std::string s1, s2;
+    async_cin >> s1;
+    async_cout << s1 << std::endl;
+    
+    async_cin.Scan(s1, s2);
+    async_cout.Print(s1, '\n', s2, '\n');
+}
+
+void sleep_print() {
+    static int i = 10;
+    while (i--) {
+        sleep(1);
+        printf("\nhello async io\n");
+    }
+}
+
+int main() {
+    flexy::IOManager iom;
+    go test;
+    go sleep_print;
+}
