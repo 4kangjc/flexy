e94d5eaac7ca3a2a30f266219ef32907950888e8
4kangjc <73515066+4kangjc@users.noreply.github.com>
2022-02-11 17:58:03 +0800

    Update http_session.h

--- flexy/http/http_session.h
+++ flexy/http/http_session.h
@@ -7,7 +7,7 @@ namespace flexy::http {
 
 class HttpSession : public SockStream {
 public:
-    using ptr = std::unique_ptr<HttpSession>;
+    using ptr = std::shared_ptr<HttpSession>;
     HttpSession(const Socket::ptr& sock, bool owner = true);
     HttpRequest::ptr recvRequest();
     int sendResponse(const HttpResponse::ptr& rsp); 
