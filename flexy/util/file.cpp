3736b215ef68f240e58a93ef7455768461d17f8f
4kangjc <1158035520@qq.com>
2022-04-16 23:08:17 +0800

    add flexy::filesystem namespace

--- flexy/util/file.cpp
+++ flexy/util/file.cpp
@@ -155,3 +155,176 @@ uint64_t FS::LastWriteTime(std::string_view filename) {
 
 
 } // namespace flexy
+
+
+namespace flexy::filesystem {
+
+template <bool v>
+std::string AbsolutePath(std::string_view filename) {
+    if (filename[0] == '/') {
+        return std::string(filename);
+    }
+    if constexpr (v) {
+        char path[512] = {0};
+        ssize_t count = readlink("/proc/self/exe", path, 512);
+        if (count <= 0) {
+            return "";
+        }
+        auto p = fs::path(path).parent_path()/filename;
+        return p;
+    }
+    return fs::absolute(filename);
+}
+
+template <bool v>
+bool Mv(std::string_view from, std::string_view to) {
+    try {
+        if constexpr (v) {
+            auto __from = AbsolutePath(from);
+            auto __to = AbsolutePath(to);
+            fs::rename(__from, __to);
+        } else {
+            fs::rename(from, to);
+        }
+    } catch (std::exception& ex) {
+        FLEXY_LOG_FMT_ERROR(g_logger, "FS Mv Except: {}", ex.what());
+        return false;
+    } catch (...) {
+        FLEXY_LOG_ERROR(g_logger) << "FS Mv Except";
+        return false;
+    }
+    return true;
+}
+
+// 暂时不提供v, Env模块转换绝对路径
+void ListAllFile(std::vector<std::string>& files, std::string_view path, 
+                std::string_view subfix) {
+    fs::path dir(path);
+    if (!fs::exists(dir)) {
+        return;
+    }
+    fs::directory_iterator di(dir);
+    for (auto& it : di) {
+        if (it.status().type() == fs::file_type::directory) {
+            ListAllFile(files, it.path().c_str(), subfix);
+        } else if (it.status().type() == fs::file_type::regular) {
+            if (subfix.empty()) {
+                files.push_back(it.path());
+            } else {
+                auto s = it.path().c_str();
+                auto len = strlen(s);
+                if (strncmp(s + len - subfix.size(), subfix.data(), subfix.size()) == 0) {
+                    files.push_back(it.path());
+                }
+            }
+        }
+    }
+}
+
+template <bool v>
+bool Mkdir(std::string_view dirname) {
+    if constexpr (v) {
+        auto s = AbsolutePath(dirname);
+        return Mkdir(s);
+    }
+    fs::path dir(dirname);
+    if (fs::exists(dir)) {
+        return true;
+    }
+    return fs::create_directories(dirname);
+}
+
+template <bool v>
+bool Rm(std::string_view path) {
+    if (v) {
+        auto s = AbsolutePath(path);
+        return Rm(s);
+    }
+    try {
+        fs::remove_all(path);
+    } catch (std::exception& ex) {
+        FLEXY_LOG_FMT_ERROR(g_logger, "FS Rm Except: {}", ex.what());
+        return false;
+    } catch (...) {
+        FLEXY_LOG_ERROR(g_logger) << "Fs Rm Except";
+        return false;
+    }
+    return true;
+}
+
+bool IsRunningPidfile(std::string_view pidfile) {
+    std::ifstream ifs(pidfile.data());
+    std::string line;
+    if (!ifs || !std::getline(ifs, line)) {
+        return false;
+    }
+    if (line.empty()) {
+        return false;
+    }
+    pid_t pid = ::atoi(line.c_str());
+    if (pid <= 1) {
+        return false;
+    }
+    if (kill(pid, 0) != 0) {
+        return false;
+    }
+    return true;
+}
+
+template <bool v>
+bool OpenForWrite(std::ofstream& ofs, std::string_view filename,
+            std::ios_base::openmode mode) {
+    if constexpr (v) {
+        return FS::OpenForWrite(ofs, AbsolutePath(filename), mode);
+    }
+    ofs.open(filename.data(), mode);
+    if (!ofs.is_open()) {
+        fs::path file(filename);
+        fs::create_directories(file.parent_path());
+        ofs.open(filename.data(), mode);
+    }
+    return ofs.is_open();
+}
+
+template <bool v>
+bool OpenForRead(std::ifstream& ifs, std::string_view filename,
+            std::ios_base::openmode mode) {
+    if constexpr (v) {
+        return FS::OpenForRead(ifs, AbsolutePath(filename), mode);
+    }
+    ifs.open(filename.data(), mode);
+    return ifs.is_open();
+}
+
+template <bool v>
+uint64_t LastWriteTime(std::string_view filename) {
+    if constexpr (v) {
+        return FS::LastWriteTime(AbsolutePath(filename));
+    }
+    auto ftime = fs::last_write_time(filename);
+    return std::chrono::duration_cast<std::chrono::microseconds>(ftime.time_since_epoch()).count();
+}
+
+namespace {
+
+// 实例化模板 为了编译通过 并不调用
+void __FILE__INIT__() {
+    AbsolutePath<false>("");
+    AbsolutePath<true>("");
+    LastWriteTime<false>("");
+    LastWriteTime<true>("");
+    Mkdir<false>("");
+    Mkdir<true>("");
+    Mv<false>("", "");
+    Mv<true>("", "");
+    std::ifstream ifs;
+    OpenForRead<true>(ifs, "", std::ios::app);
+    OpenForRead<false>(ifs, "", std::ios::app);
+    std::ofstream ofs;
+    OpenForWrite<true>(ofs, "", std::ios::app);
+    OpenForWrite<false>(ofs, "", std::ios::app);
+}
+
+}
+
+} // namespace flexy file system
--- flexy/util/file.h
+++ flexy/util/file.h
@@ -32,3 +32,45 @@ struct FS {
 
 
 } // namespace flexy
+
+// 与 std::filesystem的不同地方在于 
+// 若`v`为`true`那么 path 会转换为 绝对路径 
+// 且 绝对路径不会随着工作目录的变更而变更 始终是相对于`bin`目录下
+namespace flexy::filesystem {
+
+template <bool v = true>
+std::string AbsolutePath(std::string_view filename);
+
+// 列出path路径下的所有以subfix结尾的文件
+void ListAllFile(std::vector<std::string>& files, std::string_view path, 
+                 std::string_view subfix);
+
+// 建立文件夹
+template <bool v = false>
+bool Mkdir(std::string_view dirname);
+
+// 删除文件或文件夹
+template <bool v = false>
+bool Rm(std::string_view path);
+
+// 移动或重命名文件或文件夹
+template <bool v = false>
+bool Mv(std::string_view from, std::string_view to);
+
+bool IsRunningPidfile(std::string_view pidfile);
+
+// open不支持std::string_view, 注意std::string_view 以 \\0 结尾
+template <bool v = false>
+bool OpenForRead(std::ifstream& ifs, std::string_view  filename,
+                 std::ios_base::openmode mode);
+
+// open不支持std::string_view, 注意std::string_view 以 \\0 结尾
+template <bool v = false>
+bool OpenForWrite(std::ofstream& ofs, std::string_view filename,
+                  std::ios_base::openmode mode);
+
+// 上次文件修改时间
+template <bool v = false>
+uint64_t LastWriteTime(std::string_view filename);
+
+} // namespace flexy file system
