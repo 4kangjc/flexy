9ae8eabb52f05a1f48afb073f84c6cb7aa5f2390
4kangjc <1158035520@qq.com>
2022-04-25 21:23:39 +0800

    add getRemoteAddress and getLocalAddress

--- flexy/stream/socket_stream.h
+++ flexy/stream/socket_stream.h
@@ -6,6 +6,7 @@ namespace flexy {
 
 class SockStream : public Stream {
 public:
+    using ptr = std::shared_ptr<SockStream>;
     SockStream(const Socket::ptr& sock, bool owner);
     ~SockStream();
     /*virtual*/ ssize_t read(void* buffer, size_t length) override;
@@ -16,6 +17,21 @@ public:
 
     auto& getSocket() const { return sock_; }
     bool isConnected() const;
+
+    Address::ptr getLocalAddress() const {
+        return sock_ ? sock_->getLocalAddress() : nullptr;
+    }
+    Address::ptr getRemoteAddress() const {
+        return sock_ ? sock_->getRemoteAddress() : nullptr;
+    }
+    std::string getLocalAddressString() const {
+        auto localAddress = getLocalAddress();
+        return localAddress ? localAddress->toString() : "";
+    }
+    std::string getRemoteAddressString() const {
+        auto remoteAddress = getRemoteAddress();
+        return remoteAddress ? remoteAddress->toString() : "";
+    }
 protected:
     Socket::ptr sock_;
     bool owner_;
