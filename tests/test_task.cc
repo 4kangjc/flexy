1f4942b75c03a79685b539c478d20327ddf4b8f8
4kangjc <1158035520@qq.com>
2022-04-27 22:49:35 +0800

    update test task

--- tests/test_task.cc
+++ tests/test_task.cc
@@ -5,6 +5,10 @@ static auto&& g_logger = FLEXY_LOG_ROOT();
 
 using namespace flexy::detail;
 
+// using task = __task_virtual;
+// using task = __task_function;
+using task = __task_template;
+
 void print(const char* s) {
     FLEXY_LOG_FMT_INFO(g_logger, "{}", s);
 }
@@ -15,33 +19,40 @@ struct Add {
     }
 };
 
-void test2(__task&& cb) {
+void test2(task&& cb) {
     cb();
 }
 
 template <typename... Args>
 void test1(Args&&... args) {
-    // __task cb(std::forward<Args>(args)...);
-    return test2(__task(std::forward<Args>(args)...));
+    // task cb(std::forward<Args>(args)...);
+    return test2(task(std::forward<Args>(args)...));
     // return test2(std::move(cb));
 }
 
+void test_unique(std::unique_ptr<int>&& uptr) {
+    FLEXY_LOG_DEBUG(g_logger) << "unique_ptr value = " << *uptr;
+}
 
 int main() {
-    __task t1(print, "hello __task");
-    __task t2(print, "hello __task again");
+    task t1(print, "hello task");
+    task t2(print, "hello task again");
     t1();
     t2();
-    __task t3([]() { FLEXY_LOG_FMT_INFO(g_logger, "test lamada"); });
+    task t3([]() { FLEXY_LOG_FMT_INFO(g_logger, "test lamada"); });
     t3();
     int i = 2;
-    __task t4([&i]() { ++i; });
+    task t4([&i]() { ++i; });
     t4();
     FLEXY_LOG_FMT_INFO(g_logger, "i = {}", i);
     Add a;
-    __task t5(&Add::operator(), a, 1, 2);
-    __task t6(a, 2, 3);
+    task t5(&Add::operator(), a, 1, 2);
+    task t6(a, 2, 3);
     t5(); t6();
 
     test1(print, "Hello std::forward");
+
+    std::unique_ptr<int> uptr(new int(2));
+    task t7(test_unique, std::move(uptr));
+    t7();
 }
