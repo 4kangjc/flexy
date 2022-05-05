382a4f1b852361d393b98f8ec97f56eed1e1cb99
4kangjc <1158035520@qq.com>
2022-05-05 21:37:04 +0800

    update test task

--- tests/test_task.cc
+++ tests/test_task.cc
@@ -1,5 +1,6 @@
 #include <flexy/util/task.h>
 #include <flexy/util/log.h>
+#include <gtest/gtest.h>
 
 static auto&& g_logger = FLEXY_LOG_ROOT();
 
@@ -7,63 +8,131 @@ using namespace flexy::detail;
 
 // using task = __task_virtual;
 // using task = __task_function;
-using task = __task_template;
+// using task = __task_template;
 
-void print(const char* s) {
-    FLEXY_LOG_FMT_INFO(g_logger, "{}", s);
+using task = __task_Function;
+
+TEST(__Task, Lambda) {
+    int ans = 0;
+    task t1([](int a, int b, int& ans) {
+        ans = a + b;
+    }, 1, 2, std::ref(ans));
+    t1();
+    ASSERT_EQ(ans, 3);
+    
+    task t2([](const char* s) {
+        ASSERT_EQ(s, "Hello World");
+    }, "Hello World");
+    t2();
+    t2();
+
+    task t3([&ans](int a, int b) {
+        ans = a * b;
+    }, 4, 5);
+    t3();
+    ASSERT_EQ(ans, 20);
+
+    task t5([ans]() mutable {
+        ans /= 4;
+    });
+    ASSERT_EQ(ans, 20);
+
+    task t6([](auto&& func){
+        func();
+    }, std::move(t2));
+
+    task t7([](auto&& first, auto&&... other){
+        auto res = (first + ... + other);
+        ASSERT_EQ(res, 55);
+    }, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    t7();
 }
 
-struct Add {
-    void operator() (int a, int b) {
-        FLEXY_LOG_FMT_INFO(g_logger, "{} + {} = {}", a, b, a + b);
-    }
-};
+TEST(__Task, MemberMethod) {
+    // noncopyable plus
+    struct plus {
+        int ans = 0;
 
-void test2(task&& cb) {
+        void operator()(int a, int b) {
+            ans = a + b;
+        }
+        plus(const plus&) = delete;
+        plus() = default;
+        plus(plus&&) = default;
+    };
+
+    plus p;
+    task t1(std::ref(p), 4, 5);
+    t1();
+    ASSERT_EQ(p.ans, 9);
+
+    task t2(&plus::operator(), &p, 1, 2);
+    t2();
+    ASSERT_EQ(p.ans, 3);
+
+    task t3(std::move(p), 0, 0);
+    t3();
+
+    struct Worker {
+        void operator()() const {}
+
+        void Work(std::string_view hello) {
+            ASSERT_EQ(hello, "hello");
+        }
+        Worker() = default;
+        Worker(const Worker&) = default;
+    };
+
+    const Worker w;
+    task t4(w);
+    task t5(&Worker::operator(), &w);
+
+    Worker ww;
+    task t6(w);
+    task t7(&Worker::Work, &ww, "hello");
+}
+
+void test_unique(std::unique_ptr<int>&& uptr) {
+    ASSERT_EQ(*uptr, 0x99);
+}
+
+void Invoke(task&& cb) {
     cb();
 }
 
 template <typename... Args>
-void test1(Args&&... args) {
-    // task cb(std::forward<Args>(args)...);
-    return test2(task(std::forward<Args>(args)...));
-    // return test2(std::move(cb));
+void Forward(Args&&... args) {
+    return Invoke(task(std::forward<Args>(args)...));
 }
 
-void test_unique(std::unique_ptr<int>&& uptr) {
-    FLEXY_LOG_DEBUG(g_logger) << "unique_ptr value = " << *uptr;
+template <class T>
+void max(T a, T b, T& ans) {
+    ans = a < b ? a : b;
 }
 
-struct NonCopyable {
- 	void operator()() {}
-    NonCopyable() = default;
-	NonCopyable(NonCopyable&&) = default;
-};
+TEST(__Task, Function) {
+    struct NonCopyable {
+        void operator()() {}
+        NonCopyable() = default;
+        NonCopyable(NonCopyable&&) = default;
+    };
 
-int main() {
-    task t1(print, "hello task");
-    task t2(print, "hello task again");
+    NonCopyable nc;
+    task t1 = std::move(nc);
     t1();
+
+    task t2(&test_unique, std::make_unique<int>(0x99));
     t2();
-    task t3([]() { FLEXY_LOG_FMT_INFO(g_logger, "test lamada"); });
+
+    Forward(test_unique, std::make_unique<int>(0x99));
+
+    int ans = 0;
+    task t3(&max<int>, 0x99, -2, std::ref(ans));
     t3();
-    int i = 2;
-    task t4([&i]() { ++i; });
-    t4();
-    FLEXY_LOG_FMT_INFO(g_logger, "i = {}", i);
-    Add a;
-    task t5(&Add::operator(), a, 1, 2);
-    task t6(a, 2, 3);
-    t5(); t6();
+    ASSERT_EQ(ans, -2);
+}
 
-    test1(print, "Hello std::forward");
-
-    std::unique_ptr<int> uptr(new int(2));
-    task t7(test_unique, std::move(uptr));
-    t7();
-    NonCopyable non;
-    task t8(std::move(non));  // ok
-    // task t8(std::ref(non));   // ok
-    // task t8(non);               // compile fail
-    t8();
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
